66de74647f79ab838faa15dccb1a105de440761f
thesammet <thesammet@gmail.com>
2024-10-10 17:59:33 +0300

    addition: supsis-android-preview library

---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Supsis Widget"
+include(":app")
+include(":supsis-android-preview")
---
+++ supsis-android-preview/src/main/java/com/supsis/supsis_android_preview/SupsisUtils.kt
@@ -0,0 +1,2 @@
+package com.supsis.supsis_android_preview
+
