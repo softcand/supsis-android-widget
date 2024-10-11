a0f3e23985f4ce46255f87c5d44137b2bd12b938
thesammet <thesammet@gmail.com>
2024-10-11 14:26:30 +0300

    refactoring: jitpack config

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,5 +64,5 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-    implementation("com.github.softcand:supsis-android-widget:1.0.0")
+    implementation(project(":supsis-android-preview"))
 }
